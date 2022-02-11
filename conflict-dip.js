cedcd315da751507565cb0a7728122ab584c4903
dipSaha3232 <bsse1001@iit.du.ac.bd>
2022-02-11 13:22:53 +0600

    modified in dip branch

--- conflict-dip.js
+++ conflict-dip.js
@@ -1,8 +1,3 @@
 //Dip Branch code
 
-const num1 = 5;
-const num2 = 3;
-
-const sum = num1 + num2;
-
-console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+console.log("This is dip branch code");
