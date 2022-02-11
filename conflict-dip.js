e92e0894827e14d1fc3d869b893a71a9e5e4ba82
dipSaha3232 <bsse1001@iit.du.ac.bd>
2022-02-11 13:28:51 +0600

    updated main branch

--- conflict-dip.js
+++ conflict-dip.js
@@ -1,15 +1,9 @@
 //main Branch code
 
-const points = [40, 100, 1, 5, 25, 10];
-
-for (let i = points.length -1; i > 0; i--) {
-  let j = Math.floor(Math.random() * i)
-  let k = points[i]
-  points[i] = points[j]
-  points[j] = k
-}
 
 
+ksjdfhksjdhksdkfh
 jfjshf
 adkdkfh
 fsdjkhjk
+asdhj
