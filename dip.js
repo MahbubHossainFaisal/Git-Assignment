98eafe3f8c77b9f57f18a3aa690cd5012f41ff97
dipSaha3232 <bsse1001@iit.du.ac.bd>
2022-02-11 12:48:52 +0600

    modified in Dip branch

--- dip.js
+++ dip.js
@@ -1,5 +1,3 @@
-// Now I am in main branch
-// I am creating a conflict with 'dip.js' file in 'Dip' Branch
-
-let d = new Date();
-alert("Today's date is " + d);
+iuagdkfgg
+fdgsdjgfsjdfj
+sdjgfjhsdg
